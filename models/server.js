b6479a3279faef3c12186fc30dc0d1295c76b415
Marlon Gómez <marlonjulian15@gmail.com>
2021-08-09 21:09:02 -0500

    add cors support

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
+const cors = require("cors");
 
 class Server {
   constructor() {
@@ -16,6 +17,8 @@ class Server {
   }
   middlewares() {
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+    //CORS
+    this.app.use(cors());
   }
   configSockets() {
     new Sockets(this.io);
